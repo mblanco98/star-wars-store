87b8beb1b7a59e4476b92ebe9b2fa42dc7acea41
Manuel Blanco <manuel.22.lamas@gmail.com>
2019-06-04 15:21:54 -0500

    starting files

---
+++ components/List.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    list component
+  </div>
+</template>
+<script>
+export default {
+  name: 'List'
+}
+</script>
+<style lang="scss" scoped>
+@import '../assets/scss/components/list.scss';
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="container">
+    <h1 class="title">
+      DRAG N' DROP API
+    </h1>
+  </section>
+</template>
+
+<script>
+import List from '~/components/List'
+
+export default {
+  components: {
+    List
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/scss/pages/index.scss';
+</style>
